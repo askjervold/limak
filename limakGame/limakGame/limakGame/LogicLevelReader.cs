b9425862fa3c6f0bd24c943579de6d4d2bf497f5
Audun Skjervold <audun.xetrov@gmail.com>
2013-04-17 12:48:26 +0200

    Minor changes + working on createPlatforms()

--- limakGame/limakGame/limakGame/LogicLevelReader.cs
+++ limakGame/limakGame/limakGame/LogicLevelReader.cs
@@ -16,15 +16,24 @@ namespace limakGame
         //the world object to be passed in through the constructor
         World _world;
 
+        //variables to convert between units
+        const float unitToPixel = 60.0f;
+        const float pixelToUnit = 1 / unitToPixel;
+
+        //height and length of level, found by readFile()
+        private int levelHeight;
+        private int levelWidth;
+
         //types of characters found in the .txt file
         private string _ground = "#";
+        private string _platform = "_";
         private string _player = "|";
         private string _enemy = "@";
 
         //types of bodies created and returned to the draw class.
-        private Body _groundBody;
-        private Body[] _platforms;
-        private Body[] _enemies;
+        private Body _groundBody; 
+        private List<Body> _platforms;
+        private List<Body> _enemies;
         
         //an array of string where each element is each line in the .txt file.
         string[] text;
@@ -36,6 +45,10 @@ namespace limakGame
 
             // Temporary world for testing purposes
             _world = new World(new Vector2(0, 9.81f));
+
+            _platforms = new List<Body>();
+            _enemies = new List<Body>();
+            
         }
 
         //this reads the given file, we can change which file by inputting a different parameter.
@@ -47,6 +60,10 @@ namespace limakGame
             else
                 text = System.IO.File.ReadAllLines(file + ".txt");
 
+            //registers the dimensions of the level
+            levelHeight = text.Length;
+            levelWidth = text[levelHeight - 1].Length;
+
             createGround();
             //createPlatforms(); //TODO
             createEnemies();
@@ -54,12 +71,14 @@ namespace limakGame
             
         }
 
+        //assumes no holes in the ground, creating a single body representing the entire ground floor
         public void createGround()
         {
             //this method assumes that the ground is always on the lowest point, which all map should be.
-            int first = text[text.Length-1].IndexOf(_ground);
-            int last = text[text.Length-1].LastIndexOf(_ground);
-            string str2 = text[text.Length-1].Substring(first, last - first); // gets the width of level.
+            int levelHeight = text.Length - 1;
+            int first = text[levelHeight].IndexOf(_ground);
+            int last = text[levelHeight].LastIndexOf(_ground);
+            string str2 = text[levelHeight].Substring(first, last - first); // gets the width of level.
             //_groundSprite = Content.Load<Texture2D>("groundSprite"); // 512px x 64px =>   8m x 1m
 
 
@@ -67,7 +86,8 @@ namespace limakGame
             _groundBody = BodyFactory.CreateRectangle(_world, str2.Length, 1, 1);
 
             //non-movable object.
-            _groundBody.IsStatic = true;
+            _groundBody.BodyType = BodyType.Static;
+
             //some copy-paste code
             _groundBody.Restitution = 0.3f;
             _groundBody.Friction = 0.5f;
@@ -76,7 +96,22 @@ namespace limakGame
 
         public void createPlatforms()
         {
+            // iterates through the entire level file and checks for a "_" which indicates a platform. If one is found, a body is added to the platform list
+            for (int i = 0; i < levelHeight; i++)
+            {
+                for (int j = 0; j < levelWidth; j++)
+                {
+                    if (text[i][j].Equals("_"))
+                    {
+                        _platforms.Add(new Body(_world));
+                    }
+                    
+                }
 
+                
+            }
+
+            
         }
 
         public void createEnemies()
@@ -90,13 +125,13 @@ namespace limakGame
         }
 
 
-        public Body[] Platforms
+        public List<Body> Platforms
         {
             get { return _platforms; }
         }
 
 
-        public Body[] Enemies
+        public List<Body> Enemies
         {
             get { return _enemies; }            
         }

dc773d832726f41256e92fef295a0c762e21339a
Shouryu <instagibno@gmail.com>
2013-04-23 00:36:37 +0200

    floors have holes in them, still need some tweaking - positioning and adding the correctly

--- limakGame/limakGame/limakGame/LogicLevelReader.cs
+++ limakGame/limakGame/limakGame/LogicLevelReader.cs
@@ -31,8 +31,9 @@ namespace limakGame
         private string _enemy = "@";
 
         //types of bodies created and returned to the draw class.
-        private Body _groundBody;
+        private Body _groundBody1;
         private List<Body> _platforms;
+        private List<Body> _groundBody;
         private List<GameObject> _enemies;
         private List<Vector2> _enemyPositions;
         private GameObject _player1Object;
@@ -50,9 +51,11 @@ namespace limakGame
         {
             this.world = world;
 
+            _groundBody = new List<Body>();
             _platforms = new List<Body>();
             _enemies = new List<GameObject>();
             _enemyPositions = new List<Vector2>();
+
 
         }
 
@@ -75,13 +78,11 @@ namespace limakGame
             createPlatforms(); //TODO
             createEnemies();
             createGround();
-            Console.WriteLine("ENEMY POS");
             createEnemyPositions();
-            Console.WriteLine("ENEMY POS DONE");
 
 
         }
-
+        //this might not be used.
         public void createPlayer()
         {
             Vector2 start = new Vector2(0, 0);
@@ -94,28 +95,62 @@ namespace limakGame
 
         }
 
+        
         public void createGround()
         //assumes no holes in the ground, creating a single body representing the entire ground floor
         {
             //this method assumes that the ground is always on the lowest point, which all map should be.
             int levelHeight = text.Length - 1;
-            int first = text[levelHeight].IndexOf(_ground);
-            int last = text[levelHeight].LastIndexOf(_ground);
-            string str2 = text[levelHeight].Substring(first, last - first); // gets the width of level.
+            //int first = text[levelHeight].IndexOf(_ground);
+            //int last = text[levelHeight].LastIndexOf(_ground);
+            //string str2 = text[levelHeight].Substring(first, last - first); // gets the width of level.
             //_groundSprite = Content.Load<Texture2D>("groundSprite"); // 512px x 64px =>   8m x 1m
 
 
-            //create ground fixture, it assumes that the ground doesn't have any holes in it
-            _groundBody = BodyFactory.CreateRectangle(world, str2.Length, 1, 1, new Vector2(0 / 2, levelHeight - 1));
-            //_groundBody = BodyFactory.CreateRectangle(world, 60.0f, 1.0f, 1.0f);
 
 
-            //non-movable object.
-            _groundBody.BodyType = BodyType.Static;
+            int start = 0;
+            bool lastFloor = false;
 
-            //some copy-paste code
-            _groundBody.Restitution = 0.3f;
-            _groundBody.Friction = 0.5f;
+
+            for (int i = 0; i < _levelWidth; i++)
+            {
+                if (text[levelHeight][i] == '#' && !lastFloor)
+                {
+                    start = i;
+                    lastFloor = true;
+                }
+                if ((text[levelHeight][i] == ' ' || text[levelHeight][i] == 'n') && lastFloor)
+                {
+                    Body temp = BodyFactory.CreateRectangle(world, (i - 1) - start, 1, 1, new Vector2(((i - 1) - start)/2.0f, levelHeight - 1));
+                    //non-movable object.
+                    temp.BodyType = BodyType.Static;
+
+                    //some copy-paste code
+                    temp.Restitution = 0.3f;
+                    temp.Friction = 0.5f;
+                    
+                    _groundBody.Add(temp);
+                }
+
+            }
+            
+
+            
+
+
+
+            ////create ground fixture, it assumes that the ground doesn't have any holes in it
+            //_groundBody1 = BodyFactory.CreateRectangle(world, str2.Length, 1, 1, new Vector2(levelWidth/ 2, levelHeight - 1));
+            ////_groundBody = BodyFactory.CreateRectangle(world, 60.0f, 1.0f, 1.0f);
+
+
+            ////non-movable object.
+            //_groundBody1.BodyType = BodyType.Static;
+
+            ////some copy-paste code
+            //_groundBody1.Restitution = 0.3f;
+            //_groundBody1.Friction = 0.5f;
 
         }
 
@@ -155,10 +190,8 @@ namespace limakGame
 
 
                 }
-                Console.WriteLine();
 
             }
-            Console.Write(_platforms.Count);
 
 
         }
@@ -185,41 +218,24 @@ namespace limakGame
         {
             for (int i = 0; i < _levelHeight; i++)
             {
-
-                /*try
+                int j = 0;
+                try//in case we get an error
                 {
-                    int j = 0;
-                    while ((i = text[i].IndexOf('@', j)) != -1 && j < _levelWidth)
+                    while (((j=text[i].IndexOf('@', j)) != -1) && j < _levelWidth)
                     {
-                        Console.WriteLine("AAAA");
-                        // C.
-                        // Print out the substring.
-                        _enemyPositions.Add(new Vector2(i, j));
-
-                        // D.
-                        // Increment the index.
+                        _enemyPositions.Add(new Vector2(j, i));
                         j++;
                     }
 
                 }
-
-                 /*   if (text[i][j].Equals("n")) Console.WriteLine("NEWLINE");
-                    //Console.Write("\tI:" + i + " J:" + j + " tegn:" + text[i][j]);
-                    if (text[i][j].Equals(_enemy))
-                    {
-                        _enemyPositions.Add(new Vector2(i, j));
-                    }
-                }*/
-              /*  catch
+                catch
                 {
                 }
 
-            */
+            
 
             }
-            Vector2 a = new Vector2(0, 2f);
-            _enemyPositions.Add(a);
-            Console.WriteLine("\n\n\n\n\n" + _enemyPositions.Count);
+            
         }
 
         public List<Vector2> getEnemyPos
@@ -227,7 +243,12 @@ namespace limakGame
             get { return _enemyPositions; }
         }
 
-        public Body Ground
+        public Body Ground1
+        {
+            get { return _groundBody1; }
+        }
+
+        public List<Body> Ground
         {
             get { return _groundBody; }
         }

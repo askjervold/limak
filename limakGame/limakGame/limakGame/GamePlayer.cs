800528c356e1701f4bcc27588c8b5d1a17a1fe23
Audun Skjervold <audun.xetrov@gmail.com>
2013-04-25 14:53:37 +0200

    Moved collision handlers into the GamePlayer class

--- limakGame/limakGame/limakGame/GamePlayer.cs
+++ limakGame/limakGame/limakGame/GamePlayer.cs
@@ -8,6 +8,8 @@ using Microsoft.Xna.Framework.Graphics;
 
 using FarseerPhysics.Dynamics;
 using FarseerPhysics.Collision.Shapes;
+using FarseerPhysics.Dynamics.Contacts;
+using FarseerPhysics.Common;
 
 namespace limakGame
 {
@@ -15,12 +17,14 @@ namespace limakGame
     {
         private int score;
         private PlayerIndex playerIndex;
+        private Game game;
 
 
         public GamePlayer(Game game, World world, Vector2 position, Vector2 size, SpriteAnimation animation, PlayerIndex playerIndex) 
             : base(game, world, position, size, animation)
         {
             this.playerIndex = playerIndex;
+            this.game = game;
         }
 
 
@@ -46,5 +50,127 @@ namespace limakGame
             // Do more stuff to make the player alive again
         }
 
+
+        // Collision handlers
+        public bool CollisionWithEnemy(Fixture f1, Fixture f2, Contact contact)
+        {
+            Vector2 normal;
+            FixedArray2<Vector2> points;
+            contact.GetWorldManifold(out normal, out points);
+
+            foreach (IGameComponent comp in this.game.Components)
+            {
+                GameEnemy enemy = comp as GameEnemy;
+                if (enemy != null)
+                {
+                    if (enemy.getFixture() == f2)
+                    {
+
+                        if ((Math.Abs(normal.Y) > Math.Abs(normal.X)) && (normal.Y < 0))    // The contact is coming from above
+                        {
+                            enemy.Die();
+                            this.increaseScore(10);
+                            this.Jump();
+                        }
+                        else
+                        {
+                            this.Die();
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+
+            return true;
+        }
+
+        public bool PickUpCoin(Fixture f1, Fixture f2, Contact contact)
+        {
+            foreach (IGameComponent comp in this.game.Components)
+            {
+                GameCoin coin = comp as GameCoin;
+                if (coin != null)
+                {
+                    if (coin.getFixture() == f2)
+                    {
+                        //     Components.Remove(coin);
+                        this.increaseScore(1);
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public bool PlayerPlayerCollision(Fixture f1, Fixture f2, Contact contact)
+        {
+            Vector2 normal;
+            FixedArray2<Vector2> points;
+            contact.GetWorldManifold(out normal, out points);
+
+            foreach (IGameComponent comp in this.game.Components)
+            {
+                GamePlayer player = comp as GamePlayer;
+                if (player != null)
+                {
+                    if ((player.getFixture() == f2))
+                    {
+                        if ((Math.Abs(normal.Y) > Math.Abs(normal.X)) && (normal.Y < 0))
+                        {
+                            if (player.isDead)
+                            {
+                                player.Revive();
+                            }
+                        }
+                    }
+                }
+            }
+
+            /*
+            if (player1.getFixture() == f2)
+            {
+                if ((Math.Abs(normal.Y) > Math.Abs(normal.X)) && (normal.Y < 0))
+                {
+                    if (player1.IsDead()) // Shouldn't this work with just "if (player1.isDead)"?
+                    {
+                        player1.Revive();
+                    }
+                }
+            }
+
+            else if (player2.getFixture() == f2)
+            {
+                if ((Math.Abs(normal.Y) > Math.Abs(normal.X)) && (normal.Y < 0))
+                {
+                    if (player2.IsDead())
+                    {
+                        player2.Revive();
+                    }
+                }
+            }
+            */
+             
+            return true;
+        }
+        
+        /*public bool PlayerFinish(Fixture f1, Fixture f2, Contact contact)   // Change all references to coins into flag/finish
+        {
+            foreach (IGameComponent comp in this.game.Components)
+            {
+                GameCoin coin = comp as GameCoin;
+                if (coin != null)
+                {
+                    if (coin.getFixture() == f2)
+                    {
+                        this.increaseScore(50);
+                        // Should end the level
+                    }
+                }
+            }
+
+            return true;
+        }*/
     }
 }

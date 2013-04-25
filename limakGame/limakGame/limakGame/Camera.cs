08779be21f5ff3341cbe66eb5c73eb60ecacc49c
jahov <jakob.hovland@gmail.com>
2013-04-25 17:04:49 +0200

    Camera tracks both players

--- limakGame/limakGame/limakGame/Camera.cs
+++ limakGame/limakGame/limakGame/Camera.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.Xna.Framework;
+using System;
 
 namespace limakGame
 {
     public class Camera
     {
         private const float PixelsPerMeter = Convert.PixelsPerMeter;
-        
+
         private RectangleF m_WorldViewport;
         private RectangleF m_ScreenViewport;
 
@@ -40,27 +41,52 @@ namespace limakGame
     public class CameraMan : GameComponent
     {
         private Camera m_Camera;
-        private GameObject m_Star;
+        private GameObject m_Star1;
+        private GameObject m_Star2;
         private int m_GroundLevel = 12;
-        
-        public CameraMan(Game game, Camera camera, GameObject star)
+
+        public CameraMan(Game game, Camera camera, GameObject star1, GameObject star2)
             : base(game)
         {
             m_Camera = camera;
-            m_Star = star;
+            m_Star1 = star1;
+            m_Star2 = star2;
         }
 
         public Camera Camera
         {
-            get { return m_Camera; } 
+            get { return m_Camera; }
         }
 
         public override void Update(GameTime gameTime)
         {
-            float height = MathHelper.Clamp((m_GroundLevel - m_Star.Position.Y) * 2.0f, 8.0f, 80.0f);
-            float width = (Convert.ToMeters(m_Camera.ScreenViewport.Width) * height) / Convert.ToMeters(m_Camera.ScreenViewport.Height);
-            float x = m_Star.Position.X - (width / 2.0f);
-            float y = m_Star.Position.Y - (height / 2.0f);
+            float x = (m_Star1.Position.X + m_Star2.Position.X) / 2.0f;
+            float y = (m_Star1.Position.Y + m_Star2.Position.Y) / 2.0f;
+            float width = Math.Abs(m_Star2.Position.X - m_Star1.Position.X) + m_Star1.Size.X + 12.0f;
+            float height = Math.Abs(m_Star2.Position.Y - m_Star1.Position.Y) + m_Star1.Size.Y + 12.0f;
+
+            float screenAspectRatio = 820.0f / 460.0f;
+            float aspectRatio = width / height;
+            float a = screenAspectRatio * (height / width);
+
+            if (a < 1)
+                height /= a;
+            else if (a > 1)
+                width *= a;
+
+            float minWidth = 20.0f;
+            if (width < minWidth)
+            {
+                float scale = minWidth / width;
+                width *= scale;
+                height *= scale;
+            }
+
+            x -= (width / 2.0f);
+            y -= (height / 2.0f);
+
+            if (y > 11.0f - height)
+                y -= (y + height - 11.0f);
 
             m_Camera.WorldViewport = new RectangleF(x, y, width, height);
         }
